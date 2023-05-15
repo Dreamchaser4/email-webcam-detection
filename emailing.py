f1db5e32a7f9cfbc1fed650314a58ed130c5f024
Dreamchaser4 <mchris8075@gmail.com>
2023-05-15 22:01:56 +0100

    Threading

--- emailing.py
+++ emailing.py
@@ -6,6 +6,7 @@ SENDER ="stephenjohnson8075@gmail.com"
 PASSWORD ="lyclloreysqtgsnz"
 RECEIVER =["stephenjohnson8075@gmail.com","leasmith4040@gmail.com", "mchris8075@gmail.com"]
 def send_email(image_path):
+    print("send_email function started")
     email_message = EmailMessage()
     email_message["Subject"] = "New customer showed up!"
     email_message.set_content("Hey, we just saw a new customer! ")
@@ -20,7 +21,7 @@ def send_email(image_path):
     gmail.login(SENDER,PASSWORD)
     gmail.sendmail(SENDER,RECEIVER,email_message.as_string())
     gmail.quit()
-
+    print("send_email function ended")
 
 if __name__ == "__main__":
-    send_email(image_path="images/45.png")
+    send_email(image_path="images/4.png")
